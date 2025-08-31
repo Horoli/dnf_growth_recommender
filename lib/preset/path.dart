c22cae6943b79444198929e3c9d978883378b389
Horoli <sunha321@gmail.com>
2025-09-01 00:09:35 +0900

    path update

--- lib/preset/path.dart
+++ lib/preset/path.dart
@@ -1,6 +1,6 @@
 const String ROUTE_SEARCH = '';
 
-const String URL_BASE = 'http://172.16.0.6:9017';
+const String URL_BASE = 'https://dnf-api.horoli.kr';
 
 const String URL_IMAGE = 'image';
 const String URL_RECOMMEND = 'recommend';
